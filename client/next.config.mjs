c148d44ed56df660993ac51370d0ac64d9be0770
Philix27 <philixbob@gmail.com>
2024-08-25 11:41:12 +0100

    add service worker files

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,11 +1,33 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+import withSerwistInit from '@serwist/next';
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
+// You may want to use a more robust revision to cache
+// files more efficiently.
+// A viable option is `git rev-parse HEAD`.
+const revision = crypto.randomUUID();
+
+const withSerwist = withSerwistInit({
+  cacheOnNavigation: true,
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js',
+  additionalPrecacheEntries: [{ url: '/~offline', revision }],
 });
 
-module.exports = withPWA({
+const rulesToProcess = [/\.m?js/, /\.(js|cjs|mjs)$/].map(String);
+const dirToIgnore = /server/;
+/** @type {import("next").NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-});
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  webpack: (config) => {
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule !== '...' && rulesToProcess.indexOf(String(rule.test)) > -1) {
+        rule.exclude = [dirToIgnore];
+      }
+      return rule;
+    });
+    return config;
+  },
+};
+
+export default withSerwist(nextConfig);

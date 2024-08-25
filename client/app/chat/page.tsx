88cc69ede7a58a9e6d532b50190a29cc2ea3ffbc
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-08-25 14:15:33 -0700

    chat: added messages to chat

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -2,12 +2,14 @@
 
 import { TextH, TextP } from '@/comps';
 import { chatData } from './data';
+import { useRouter } from 'next/navigation';
 
 export default function ChatPage() {
+  const router = useRouter();
   return (
     <div className="mt-10">
       {chatData.map((val, i) => (
-        <div key={i} className={`flex item-center justify-between px-4 py-2 mb-1 border-b-[0.5px]`}>
+        <div key={i} className={`flex item-center justify-between px-4 py-2 mb-1 border-b-[0.5px]`} onClick={()=>{router.push("/messages")}}>
           <div className="flex">
             <div className={`border-2 border-white rounded-[25px] bg-white h-[50px] w-[50px] mr-5`}>
               <img src={val.img} />
---
+++ client/app/messages/page.tsx
@@ -0,0 +1,6 @@
+"use client"
+export default function MessageChat(){
+    return <div>
+        hello
+    </div>
+}

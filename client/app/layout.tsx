3bf7cc7e90f5162d3b1a8987f8d50fca6b33b620
Philix27 <philixbob@gmail.com>
2024-09-16 23:38:04 +0100

    client: comment our qr code

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -61,6 +61,9 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="HandheldFriendly" content="true" />
+        <script>
+          
+        </script>
       </head>
       <body
         style={{ overscrollBehavior: 'none', overflow: 'hidden' }}

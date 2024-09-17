2a328c71ab33843ca9d04106c33a4fc635666c01
Philix27 <philixbob@gmail.com>
2024-09-17 07:54:10 +0100

    client: add dashboard receive page

---
+++ client/app/(messaging)/dash-receive/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { Navbar, SimpleRow, TextH } from '@/comps';
+import { use3Wagmi } from '@/lib';
+import React from 'react';
+import QRCode from 'react-qr-code';
+
+export default function SendMoneyPage() {
+  const { address } = use3Wagmi();
+  return (
+    <div>
+      <Navbar title="Send" isBack />
+
+      <div className="px-5">
+        <div className="flex items-center justify-center p-2 ">
+          <QRCode className="w-full" value={address!} viewBox={`0 0 256 256`} />
+        </div>
+        <TextH>{address}</TextH>
+        <SimpleRow left={'Display name'} right="philix27" />
+        <SimpleRow left={'Phone'} right="081082293" />
+      </div>
+    </div>
+  );
+}
+// Comp icon to copy wallet address
+// Share Icon to share generate image
--- client/app/(messaging)/dash-send/page.tsx
+++ client/app/(messaging)/dash-send/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Navbar, SimpleRow } from '@/comps';
+import { AppButton, AppInput, Navbar, SimpleRow, TextH, TextP } from '@/comps';
 import { use3Wagmi } from '@/lib';
 import React from 'react';
 import QRCode from 'react-qr-code';
@@ -10,14 +10,14 @@ export default function SendMoneyPage() {
     <div>
       <Navbar title="Send" isBack />
 
-      <div className="px-5">
-        <div className="flex items-center justify-center p-2 ">
-          {/* <QRCode className="w-full" value={address!} viewBox={`0 0 256 256`} /> */}
-        </div>
-        <SimpleRow right="Address" left={address!} />
-        <SimpleRow right="Display name" left={'Username'} />
-        <SimpleRow right="Phone" left={'081082293'} />
+      <div className="px-5 gap-y-2 w-full">
+        <AppInput control={undefined} name="wallet" place={'Enter wallet address'} />
+        <AppInput control={undefined} name="username" place={'Enter username'} />
+        <AppInput control={undefined} name="phone" place={'Enter phone number'} />
+        <AppButton>Send</AppButton>
       </div>
     </div>
   );
 }
+// Comp icon to copy wallet address
+// Share Icon to share generate image

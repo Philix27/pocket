be62b3441cddb9abf58393f2ab1cdb56f3b4f7b5
Philix27 <philixbob@gmail.com>
2024-09-18 17:49:42 +0100

    align qrcode

--- client/app/(messaging)/dash-receive/page.tsx
+++ client/app/(messaging)/dash-receive/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Navbar, SimpleRow, TextH } from '@/comps';
+import { Navbar, SimpleRow, TextH, TextP } from '@/comps';
 import { use3Wagmi } from '@/lib';
 import React from 'react';
 import QRCode from 'react-qr-code';
@@ -10,13 +10,17 @@ export default function SendMoneyPage() {
     <div>
       <Navbar title="Receive" isBack />
 
-      <div className="px-5">
-        <div className="flex items-center justify-center p-2 ">
-          {/* <QRCode className="w-full" value={address!} viewBox={`0 0 256 256`} /> */}
+      <div className="px-5 h-full flex flex-col items-center justify-center w-full">
+        <div className="flex items-center justify-center my-4 ">
+          <QRCode className="w-full bg-white p-2 rounded-md" value={address!} viewBox={`0 0 256 256`} />
         </div>
-        <TextH>{address}</TextH>
-        <SimpleRow left={'Display name'} right="philix27" />
-        <SimpleRow left={'Phone'} right="081082293" />
+        <div className="w-[70%] flex items-center justify-center  bg-card rounded-lg p-2 break-words my-4">
+          <TextP className="text-center truncate ">{address}</TextP>
+        </div>
+        <div className="w-full">
+          <SimpleRow left={'Display name'} right="philix27" />
+          <SimpleRow left={'Phone'} right="081082293" />
+        </div>
       </div>
     </div>
   );

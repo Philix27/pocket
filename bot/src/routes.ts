d30465ddc1713b1a6218cac94e419c4166985b16
Philix27 <philixbob@gmail.com>
2024-09-23 13:18:44 +0100

    remove bot and yarn

--- bot/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-import { BlankEnv, BlankSchema } from "hono/types";
-import { indexRoutes } from "./bot";
-
-export function registerRoutes(app: Hono<BlankEnv, BlankSchema, "/">) {
-  const apiRoutes = app.basePath("/api").route("/", indexRoutes);
-
-  return apiRoutes;
-}

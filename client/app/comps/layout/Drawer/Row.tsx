489e8cc1337a45024a29db84d21598b864666e06
Philix27 <philixbob@gmail.com>
2024-09-27 12:32:45 +0100

    increase font-size of drawer

--- client/app/comps/layout/Drawer/Row.tsx
+++ client/app/comps/layout/Drawer/Row.tsx
@@ -4,9 +4,9 @@ import { IconType } from 'react-icons';
 export function DrawerRow(props: { title: string; icon?: IconType; onClick?: VoidFunction }) {
   const Icon = props.icon!;
   return (
-    <div className="w-full flex items-center py-2 border-b-[0.3px]" onClick={props.onClick}>
-      {props.icon && <Icon className="mr-4" size={20} />}
-      <TextP className={'font-bold'}>{props.title}</TextP>
+    <div className="w-full flex items-center py-3" onClick={props.onClick}>
+      {props.icon && <Icon className="mr-4" size={22} />}
+      <TextP className={' font-semibold text-[16px]'}>{props.title}</TextP>
     </div>
   );
 }

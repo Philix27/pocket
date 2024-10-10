0f2a12205ee1cd7d3e74dd5a3e33e4f5d0d72c43
Philix27 <philixbob@gmail.com>
2024-10-10 13:30:18 +0100

    installed husky

---
+++ client/app/pkg.ts
@@ -0,0 +1,6 @@
+import { AddressFn, TokenFn , Tokens, cUSD} from 'celo-tokens';
+
+export default function LoadingPage() {
+  const tokenId = TokenFn.addresses['42220'];
+  // const toper = AddressFn
+}
